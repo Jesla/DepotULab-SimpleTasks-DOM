5d7b081dac818b75fe4f650f5ffed6098e9c4caf
Jesla <jesslar415@gmail.com>
2016-09-02 12:36:55 -0500

    clean up

--- scripts.js
+++ scripts.js
@@ -7,87 +7,87 @@ document.addEventListener('DOMContentLoaded', function () {
     firstButton.addEventListener('click', clickAlert1);
     document.getElementById('firstObjDiv').appendChild(firstButton);
 
-function clickAlert1() {
-    alert('Objective #1 Down!');
-}
-
-// SECOND OBJECTIVE
-
-var second = document.getElementById('secondObj');
-second.addEventListener('click', clickAlert2);
-
-function clickAlert2() {
-    var textBoxInput = document.getElementById('secondObjTextBox').value;
-    if (textBoxInput.length >= 1) {
-        alert(textBoxInput);
-    } else {
-        alert('Please enter some text.')
+    function clickAlert1() {
+        alert('Objective #1 Down!');
     }
-}
 
-// THIRD OBJECTIVE
+    // SECOND OBJECTIVE
 
-var thirdObjective = document.getElementById('thirdObjDiv');
+    var second = document.getElementById('secondObj');
+    second.addEventListener('click', clickAlert2);
+
+    function clickAlert2() {
+        var textBoxInput = document.getElementById('secondObjTextBox').value;
+        if (textBoxInput.length >= 1) {
+            alert(textBoxInput);
+        } else {
+            alert('Please enter some text.')
+        }
+    }
+
+    // THIRD OBJECTIVE
+
+    var thirdObjective = document.getElementById('thirdObjDiv');
     thirdObjective.addEventListener('mouseover', colorChange);
     thirdObjective.addEventListener('mouseout', colorRevert);
 
-function colorChange() {
+    function colorChange() {
         var colorCh = document.getElementById('thirdObjDiv');
         this.style.backgroundColor = 'red';
-}
+    }
 
-function colorRevert() {
+    function colorRevert() {
         var colorRv = document.getElementById('thirdObjDiv');
         this.style.backgroundColor = '';
-}
+    }
 
-// FOURTH OBJECTIVE 
+    // FOURTH OBJECTIVE 
 
-var fourthObjective = document.createElement('p');
-fourthObjective.id = 'fourthObj';
-fourthObjective.innerText = 'This is the fourth objective.';
-fourthObjective.addEventListener('click', clickAlert4);
-document.getElementById('fourthObjDiv').appendChild(fourthObjective);
+    var fourthObjective = document.createElement('p');
+    fourthObjective.id = 'fourthObj';
+    fourthObjective.innerText = 'This is the fourth objective.';
+    fourthObjective.addEventListener('click', clickAlert4);
+    document.getElementById('fourthObjDiv').appendChild(fourthObjective);
 
-function clickAlert4() {
-    var randColor = (Math.random() * 0xFFFFFF << 0).toString(16);
-    this.style.color = '#' + randColor;
-}
+    function clickAlert4() {
+        var randColor = (Math.random() * 0xFFFFFF << 0).toString(16);
+        this.style.color = '#' + randColor;
+    }
 
-// FIFTH OBJECTIVE
+    // FIFTH OBJECTIVE
 
     var fifthButton = document.createElement('button');
     fifthButton.id = 'fifthObj';
     fifthButton.innerText = '#5';
     fifthButton.addEventListener('click', clickAlert5);
     document.getElementById('fifthObjDiv').appendChild(fifthButton);
-    
+
     var fifthTextDiv = document.createElement('div');
     fifthTextDiv.id = 'fifthObjText';
     document.getElementById('fifthObjDiv').appendChild(fifthTextDiv);
-    
+
     var fifthSpan = document.createElement("span");
     fifthSpan.id = 'fifthObjSpan';
     fifthSpan.innerText = 'Jessica Larsen';
 
-function clickAlert5() {
-    var textDiv = document.getElementById('fifthObjText').appendChild(fifthSpan);
-}
-
-// SIXTH OBJECTIVE
-
-var friendsArray = ["Stacy", "JD", "Jared", "Brian", "Kinley", "Blair", "Stephanie", "Susan", "Jim", "Mark"];
-var sixthObjClick = addEventListener('click', clickAlert6);
-
-function clickAlert6() {
-
-    for (var i = 0; i < friendsArray.length; i++) {
-        var friendList = friendsArray[i];
-        var sixthObjList = document.createElement('li');
-        sixthObjList.class = 'sixthLiList';
-        sixthObjList.innerHTML = friendList;
-        document.getElementById('sixthUl').appendChild(sixthObjList);
+    function clickAlert5() {
+        var textDiv = document.getElementById('fifthObjText').appendChild(fifthSpan);
     }
-}
 
-   }); // THESE ARE THE FINAL CLOSE TAGS FOR THE PAGE - DO NOT REMOVE
+    // SIXTH OBJECTIVE
+
+    var friendsArray = ["Stacy", "JD", "Jared", "Brian", "Kinley", "Blair", "Stephanie", "Susan", "Jim", "Mark"];
+    var sixthObjClick = addEventListener('click', clickAlert6);
+
+    function clickAlert6() {
+
+        for (var i = 0; i < friendsArray.length; i++) {
+            var friendList = friendsArray[i];
+            var sixthObjList = document.createElement('li');
+            sixthObjList.class = 'sixthLiList';
+            sixthObjList.innerHTML = friendList;
+            document.getElementById('sixthUl').appendChild(sixthObjList);
+        }
+    }
+
+}); // THESE ARE THE FINAL CLOSE TAGS FOR THE PAGE - DO NOT REMOVE
